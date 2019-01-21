75a69146dd8564f4c8af6b36b0ca2d7c66375730
user706 <39215612+user706@users.noreply.github.com>
2019-01-21 13:06:15 -0800

    wait for user

--- target_device/full_variant/main.cpp
+++ target_device/full_variant/main.cpp
@@ -3,5 +3,7 @@
 
 int main()
 {
-    std::cout << "1 + 2 == " << sum(1, 2) << std::endl;
+  std::cout << "1 + 2 == " << sum(1, 2) << std::endl;
+  std::cout << "(hit enter)" << std::endl;
+  std::cin.ignore();
 }
